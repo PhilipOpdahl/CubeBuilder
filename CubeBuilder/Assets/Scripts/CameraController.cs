efe3324a58aa3c9aad369f4221ac2562e0d6bf8f
PhilipOpdahl <Philip.opdahl@gmail.com>
2023-05-27 21:53:09 +0200

    Good changes

--- CubeBuilder/Assets/Scripts/CameraController.cs
+++ CubeBuilder/Assets/Scripts/CameraController.cs
@@ -8,6 +8,13 @@ public class CameraController : MonoBehaviour
     public float rotateSpeed = 1.0f;
     public float scrollSpeed = 10.0f;
 
+    public float minY = -10.0f;
+    public float maxY = 10.0f;
+    public float minX = -10.0f;
+    public float maxX = 10.0f;
+    public float minZ = -10.0f;
+    public float maxZ = 10.0f;
+
     private Vector3 lastMousePos;
 
     void Update()
@@ -37,6 +44,18 @@ public class CameraController : MonoBehaviour
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         Vector3 scrollMovement = new Vector3(0, -scroll * scrollSpeed, 0);
         transform.position += scrollMovement;
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, minY, maxY);
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, minX, maxX);
+        clampedPosition.z = Mathf.Clamp(clampedPosition.z, minZ, maxZ);
+        transform.position = clampedPosition;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector3((minX + maxX)/2, (minY + maxY)/2, (minZ + maxZ)/2), new Vector3(maxX - minX, maxY - minY, maxZ - minZ));
     }
 }
 
--- CubeBuilder/Assets/Scripts/CubePlacer.cs
+++ CubeBuilder/Assets/Scripts/CubePlacer.cs
@@ -8,22 +8,25 @@ public class CubePlacer : MonoBehaviour
 {
     public GameObject cubePrefab;
     public GameObject grid;
+    public Camera camera;
     public float placeRate = 0.1f;
     public Color currentColor = Color.white;
 
     private float nextPlaceTime = 0f;
     private Vector3 lastMousePosition;
+    private Vector3 lastCameraPosition;
 
     void Update()
     {
-        if(EventSystem.current.IsPointerOverGameObject())
+        if (EventSystem.current.IsPointerOverGameObject())
         {
             return;
         }
 
         Vector3 currentMousePosition = Input.mousePosition;
+        Vector3 currentCameraPosition = camera.transform.position;
 
-        if ((Input.GetMouseButtonDown(0) || (Input.GetMouseButton(0) && currentMousePosition != lastMousePosition)) && Time.time >= nextPlaceTime)
+        if (Input.GetMouseButton(0) && (Input.GetMouseButtonDown(0) || currentMousePosition != lastMousePosition || currentCameraPosition != lastCameraPosition) && Time.time >= nextPlaceTime)
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(currentMousePosition);
@@ -58,26 +61,80 @@ public class CubePlacer : MonoBehaviour
             }
         }
 
+        if (Input.GetMouseButton(1) && (Input.GetMouseButtonDown(1) || currentMousePosition != lastMousePosition || currentCameraPosition != lastCameraPosition) && Time.time >= nextPlaceTime)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(currentMousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Cube"))
+                {
+                    Destroy(hit.transform.gameObject);
+                    nextPlaceTime = Time.time + placeRate;
+                }
+            }
+        }
+
         lastMousePosition = currentMousePosition;
+        lastCameraPosition = currentCameraPosition;
     }
 
     public void ChangeColor(string colorName)
     {
         switch (colorName.ToLower())
         {
+            case "black":
+                currentColor = Color.black;
+                break;
+            case "white":
+                currentColor = Color.white;
+                break;
+            case "grey":
+                currentColor = Color.grey;
+                break;
+            case "lightgrey":
+                currentColor = new Color(0.75f, 0.75f, 0.75f, 1f); // RGB(192, 192, 192)
+                break;
+            case "darkred":
+                currentColor = new Color(0.5f, 0, 0, 1f); // RGB(128, 0, 0)
+                break;
             case "red":
                 currentColor = Color.red;
                 break;
-            case "blue":
-                currentColor = Color.blue;
-                break;
-            case "green":
-                currentColor = Color.green;
+            case "orange":
+                currentColor = new Color(1f, 0.5f, 0, 1f); // RGB(255, 127, 0)
                 break;
             case "yellow":
                 currentColor = Color.yellow;
                 break;
-            // Add more colors as needed
+            case "green":
+                currentColor = Color.green;
+                break;
+            case "darkgreen":
+                currentColor = new Color(0, 0.5f, 0, 1f); // RGB(0, 128, 0)
+                break;
+            case "lightgreen":
+                currentColor = new Color(0.5f, 1f, 0.5f, 1f); // RGB(128, 255, 128)
+                break;
+            case "blue":
+                currentColor = Color.blue;
+                break;
+            case "darkblue":
+                currentColor = new Color(0, 0, 0.5f, 1f); // RGB(0, 0, 128)
+                break;
+            case "lightblue":
+                currentColor = new Color(0.5f, 0.5f, 1f, 1f); // RGB(128, 128, 255)
+                break;
+            case "indigo":
+                currentColor = new Color(0.5f, 0, 1f, 1f); // RGB(128, 0, 255)
+                break;
+            case "purple":
+                currentColor = new Color(0.5f, 0, 0.5f, 1f); // RGB(128, 0, 128)
+                break;
+            case "lavender":
+                currentColor = new Color(0.7f, 0.5f, 0.7f, 1f); // RGB(179, 128, 179)
+                break;
             default:
                 currentColor = Color.white;
                 break;
